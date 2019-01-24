c6dddeaad8e84824046a3b3d8fb4be3f1ef023b1
Matt <sdmattbrown@gmail.com>
2019-01-24 15:39:52 -0800

    Update and rename flaw.java to Flaw.java

---
+++ src/main/java/com/srcclr/Flaw.java
@@ -0,0 +1,10 @@
+public class Flaw {
+  
+    String passwd = "test1234";
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
