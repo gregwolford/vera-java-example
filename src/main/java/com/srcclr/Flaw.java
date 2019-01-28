2116d22716a8c476e40a97aa9ff00639ee96bfe3
m-browntown <sdmattbrown@gmail.com>
2019-01-28 12:02:43 -0800

    Updated with new Flaw

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -3,6 +3,7 @@ package com.srcclr;
 public class Flaw {
   
     String passwd = "test1234a";
+    String otherpassword = "test1234";
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
