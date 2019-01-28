8d278dc21bf25f02aec136d30cdde5c3bdc9f8d4
m-browntown <sdmattbrown@gmail.com>
2019-01-28 11:56:52 -0800

    Change to Flaw

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -2,7 +2,7 @@ package com.srcclr;
 
 public class Flaw {
   
-    String passwd = "test1234";
+    String passwd = "test1234a";
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
