aaf50e24d11cb85cd62158e44df71721039bcf73
m-browntown <sdmattbrown@gmail.com>
2019-01-29 13:16:11 -0800

    Test Password Analysis

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -2,7 +2,8 @@ package com.srcclr;
 
 public class Flaw {
   
-    String passwdOther = "test1234a";
+    String passwdOther = "test1234a12331";
+    String otherpassword = "tests";
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
