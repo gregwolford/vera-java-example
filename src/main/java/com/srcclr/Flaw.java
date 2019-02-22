bc9715ac935c325c02ccca26a46d25e9eb5b5dbc
m-browntown <sdmattbrown@gmail.com>
2019-02-22 09:48:57 -0800

    Make a flaw change and test new git flow

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -1,9 +1,14 @@
 package com.srcclr;
 
+import java.util.Random;
+
 public class Flaw {
   
     String passwdOther = "test1234a12331";
     String otherpassword = "tests";
+
+    Random rand = new Random();
+    double randDouble = rand.nextDouble();
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
