637ccd4004fc2f076bfe77b0fec91aab614092cf
Matt <sdmattbrown@gmail.com>
2019-04-04 17:39:45 -0700

    Update Flaw.java

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Flaw {
   
-    //String passwdOther = "test1234a12331";
+    String passwdOther = "test1234a12331";
     //String otherpassword = "tests";
     String thing = "test";
     Random rand = new Random();
-    //double randDouble = rand.nextDouble();
+    double randDouble = rand.nextDouble();
     //double randDouble2 = rand.nextDouble();
     //double randDouble3 = rand.nextDouble();
     //double someDouble = 1.0;
