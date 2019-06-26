ecfd011e4e9a4dc30e6789222f23681eb73d290e
m-browntown <sdmattbrown@gmail.com>
2019-06-26 10:57:45 -0700

    Sync

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -1,5 +1,6 @@
 package com.srcclr;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class Flaw {
@@ -7,9 +8,10 @@ public class Flaw {
     //String otherpassword = "tests";
     String thing = "test";
     Random rand = new Random();
+    SecureRandom secRand = new SecureRandom();
     double randDouble = rand.nextDouble();
     double randDouble2 = rand.nextDouble();
-    //double randDouble3 = rand.nextDouble();
+    double secureRandDouble3 = secRand.nextDouble();
     //double someDouble = 1.0;
 
     public static void main(String[] args) {
