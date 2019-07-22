c36514c0410782f11ee3a40569ee20e0bcb80613
m-browntown <sdmattbrown@gmail.com>
2019-07-22 15:20:19 -0700

    Remove Class Files

--- src/main/java/com/srcclr/Main.java
+++ src/main/java/com/srcclr/Main.java
@@ -8,7 +8,6 @@ import java.io.ByteArrayInputStream;
 
 public class Main {
   
-  //String passwd = "test1234";
   public static void main(String[] args) {
     String candidate = args[0];
     String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
