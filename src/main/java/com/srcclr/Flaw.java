5567efaa8ded168dfad5c96b39dff32b12a4983f
m-browntown <sdmattbrown@gmail.com>
2019-11-04 14:14:49 -0800

    Test GL API

--- src/main/java/com/srcclr/Flaw.java
+++ src/main/java/com/srcclr/Flaw.java
@@ -5,15 +5,11 @@ import java.util.Random;
 
 public class Flaw {
 
-    //String otherpassword = "tests";
-    String thing = "test";
-    Random rand = new Random();
+    String password = "tests";
+    Random insecureRand = new Random();
+    double insecureRandDouble = insecureRand.nextDouble();
     SecureRandom secRand = new SecureRandom();
-    double randDouble = rand.nextDouble();
-    double randDouble2 = rand.nextDouble();
-    double randDouble3 = rand.nextDouble();
     double secureRandDouble3 = secRand.nextDouble();
-    //double someDouble = 1.0;
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
